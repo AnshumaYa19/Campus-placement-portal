fd9c19c7a3c736732a851d6b3324fbe7eb5d0d2e
AnshumaYadav19 <anuu191204@gmail.com>
2026-07-16 23:03:23 +0530

    Add start script for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,8 @@ const connectToDb = require("./src/config/database");
 
 connectToDb()
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, ()=>{
-    console.log("Server is running...");
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
